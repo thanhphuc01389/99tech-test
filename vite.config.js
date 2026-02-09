09799a2e6eaf1ddf0bcbfdd11960b03b10ad436d
thanhphuc <thanhphuc0138@gmail.com>
2026-02-09 09:09:17 +0700

    Add allowhost

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 3099,
+    allowedHosts: ['99techtest.gopost.vn', 'www.99techtest.gopost.vn'],
+  },
 })
